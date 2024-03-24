7cfc3d08d51a77a210a57737c26cd2994c741ad0
mutapakkir <151417931+mutapakkir@users.noreply.github.com>
2024-03-24 03:01:54 -0700

    Add files via upload

---
+++ dasprog_sesi6.py
@@ -0,0 +1,25 @@
+nilai = int(input('masukkan nilai anda'))
+if nilai <=50:
+    grade = 'E'
+    predikat = 'very staticfactory'
+elif nilai <=60:
+    grade = 'D'
+    predikat = 'very staticfactory' 
+   
+elif nilai <=70:
+    grade = 'C'
+    predikat = 'very staticfactory'
+   
+elif nilai <=80:
+    grade = 'B'
+    predikat = 'very staticfactory'
+   
+elif nilai <=100:
+    grade = 'A'
+    predikat ='with compliments' 
+else:
+    print('NILAI ANDA GAGL DIINPUT, COBA LAGI')
+print(nilai,grade,predikat)
+
+
+
